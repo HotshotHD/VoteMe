baa23abf2354ca2c311da2ad1e03a0fdf0ccdfa6
Kareem <hshot9930@gmail.com>
2015-11-21 09:51:28 -0400

    Update PluginListener.php

--- src/voteme/PluginListener.php
+++ src/voteme/PluginListener.php
@@ -1,1 +1,22 @@
+<?
 
+namespace voteme;
+
+use voteme\Main;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+class PluginListener implements Listener {
+  
+  public $plugin
+  
+  public function __construct(Main $plugin) {
+    $this->plugin = $plugin;
+  }
+  
+  public function onJoin(PlayerJoinEvent $event) {
+    $player = $event->getPlayer();
+    
+    $this->plugin->generatePlayerData($player);
+  }
+}
