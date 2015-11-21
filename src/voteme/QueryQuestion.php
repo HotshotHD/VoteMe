9a228ffbd2deac645030023a38aba65eb2805ac5
Kareem <hshot9930@gmail.com>
2015-11-21 10:00:46 -0400

    Update QueryQuestion.php

--- src/voteme/QueryQuestion.php
+++ src/voteme/QueryQuestion.php
@@ -1,1 +1,22 @@
+<?php
 
+namespace voteme;
+
+use pocketmine\scheduler\PluginTask;
+
+class QueryQuestion extends PluginTask {
+  
+  public function __construct(Main $plugin) {
+    parent::__construct($plugin);
+    
+    $this->plugin = $plugin;
+  }
+  
+  public function onRun($currentTick) {
+    foreach($this->plugin->getServer()->getOnlinePlayers() as $player) {
+      if($this->plugin->getPlayerLastQuestion($player) !== $this->plugin->getQuestion()) {
+        
+      }
+    }
+  }
+}
